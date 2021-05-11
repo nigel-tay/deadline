8e4decbac6ab85634641626341952752d8813e0c
nigel-tay <ninenigel@gmail.com>
2021-05-11 23:02:50 +0800

    Complete and link all layouts

--- README.md
+++ README.md
@@ -2,8 +2,14 @@
 Look for clues to choose the right path out of horror school. Will your quick thinking get you out before the deadline, or will carelessness get the better of you?
 
 Resources: 
+
 Start screen background: "https://i.ytimg.com/vi/_lNL5C5GYgQ/maxresdefault.jpg"
+
 Scary doors: "https://www.artstation.com/a970216568"
+
+Keys:"http://1.bp.blogspot.com/-L839-wKo3YE/U7cvWmw33nI/AAAAAAAAC8k/m6__-7RfECE/s1600/PILE+OF+KEYS+wm.jpg"
+
+Chair Room: "https://lh3.googleusercontent.com/proxy/e7EFzWXcUon78BeVKJ4XvaYPfdlDpLMqpeLYHPfAWXBprK8r1kZmhVNjm5KRC5Bjpr7J9vMuTgbM_CjXjKzRPjNNpi7I54CqB_oeeh88Tr9iiKPeeO4pDQWZlzROiGdfGfSXMIDSG_14UXbQIVZFw5llTQ"
+
 Staired room: "https://s.abcnews.com/images/Lifestyle/HT_abandoned_places_pcstairs_jef_140620_22x15_1600.jpg"
-Checkered room: "https://www.abc.net.au/reslib/201403/r1247764_16598720.jpg"
-Scary Monster Mouth: "https://image.pngaaa.com/221/2497221-middle.png"https://www.clipartmax.com/png/middle/449-4498890_halloween-creepy-roblox-sticker-costume-makeup-creepy-face-transparent-background.png
+
--- js/main.js
+++ js/main.js
@@ -11,15 +11,44 @@ document.querySelector("#scary").addEventListener("click", death);
 document.querySelector("#very-scary").addEventListener("click", changeSceneLevel2);
 
 // ===================================== LEVEL 2 =====================================
-// document.getElementById("#stair1").addEventListener("click", win);
-//Win to start
-document.querySelector(".win-to-start").addEventListener("click", changeSceneStart);
+document.querySelector("#atlas").addEventListener("click", changeSceneLevel3);
+
+// ===================================== LEVEL 3 =====================================
+document.querySelector("#black-face").addEventListener("click", changeSceneLevel4);
+
+// ===================================== LEVEL 4 =====================================
+document.querySelector("#level4-exit").addEventListener("click", changeSceneLevel5);
+document.querySelector("#stair1").addEventListener("click", death);
+document.querySelector("#middle-door").addEventListener("click", death);
+document.querySelector("#stair2").addEventListener("click", death);
+
+// ===================================== LEVEL 5 =====================================
+// Password sequence to be 0672
+//NUMBER PAD
+
+document.querySelector("#dp1").addEventListener("click", oneClicked);
+document.querySelector("#dp2").addEventListener("click", twoClicked);
+document.querySelector("#dp3").addEventListener("click", threeClicked);
+document.querySelector("#dp4").addEventListener("click", fourClicked);
+document.querySelector("#dp5").addEventListener("click", fiveClicked);
+document.querySelector("#dp6").addEventListener("click", sixClicked);
+document.querySelector("#dp7").addEventListener("click", sevenClicked);
+document.querySelector("#dp8").addEventListener("click", eightClicked);
+document.querySelector("#dp9").addEventListener("click", nineClicked);
+document.querySelector("#dp0").addEventListener("click", zeroClicked);
 
 // ===================================== DEATH =======================================
 //If either "Back to start" or "Quick Restart" was selected
 document.querySelector(".back-to-start").addEventListener("click", changeSceneStart);
 document.querySelector(".quick-restart").addEventListener("click", changeSceneLevel1);
-//FUNCTIONS
+
+// ===================================== WIN PAGE ====================================
+// document.getElementById("#stair1").addEventListener("click", win);
+//Win to start
+document.querySelector(".win-to-start").addEventListener("click", changeSceneStart);
+
+
+//================================== FUNCTIONS =======================================
 function changeSceneLevel1(){
 
     //Hide start screen
@@ -40,15 +69,54 @@ function changeSceneLevel1(){
 
 function changeSceneLevel2(){
 
-    //Hide start screen
+    //Hide level 1 screen
     document.querySelector("#level1-screen").style.display = "none"
     document.querySelector("#level1-screen").style.height = "0%"
     document.querySelector("#level1-screen").style.width = "0%"
 
-    //Show level 1
+    //Show level 2
     document.querySelector("#level2-screen").style.display = "block"
     document.querySelector("#level2-screen").style.height = "100%"
     document.querySelector("#level2-screen").style.width = "100%"
+}
+
+function changeSceneLevel3(){
+
+    //Hide level 1 screen
+    document.querySelector("#level2-screen").style.display = "none"
+    document.querySelector("#level2-screen").style.height = "0%"
+    document.querySelector("#level2-screen").style.width = "0%"
+
+    //Show level 2
+    document.querySelector("#level3-screen").style.display = "block"
+    document.querySelector("#level3-screen").style.height = "100%"
+    document.querySelector("#level3-screen").style.width = "100%"
+}
+
+function changeSceneLevel4(){
+
+    //Hide level 1 screen
+    document.querySelector("#level3-screen").style.display = "none"
+    document.querySelector("#level3-screen").style.height = "0%"
+    document.querySelector("#level3-screen").style.width = "0%"
+
+    //Show level 2
+    document.querySelector("#level4-screen").style.display = "block"
+    document.querySelector("#level4-screen").style.height = "100%"
+    document.querySelector("#level4-screen").style.width = "100%"
+}
+
+function changeSceneLevel5(){
+
+    //Hide start screen
+    document.querySelector("#level4-screen").style.display = "none"
+    document.querySelector("#level4-screen").style.height = "0%"
+    document.querySelector("#level4-screen").style.width = "0%"
+
+    //Show level 5
+    document.querySelector("#level5-screen").style.display = "block"
+    document.querySelector("#level5-screen").style.height = "100%"
+    document.querySelector("#level5-screen").style.width = "100%"
 }
 
 function death(){
@@ -57,6 +125,11 @@ function death(){
     document.querySelector("#level1-screen").style.display = "none"
     document.querySelector("#level1-screen").style.height = "0%"
     document.querySelector("#level1-screen").style.width = "0%"
+
+    //Hide level 4
+    document.querySelector("#level4-screen").style.display = "none"
+    document.querySelector("#level4-screen").style.height = "0%"
+    document.querySelector("#level4-screen").style.width = "0%"
 
     //Show death
     document.querySelector("#death-display").style.display = "block"
@@ -70,7 +143,7 @@ function changeSceneStart(){
     document.querySelector("#death-display").style.height = "0%"
     document.querySelector("#death-display").style.width = "0%"
 
-    //Hide win screen
+    // Hide win screen
     document.querySelector("#win").style.display = "none"
     document.querySelector("#win").style.height = "0%"
     document.querySelector("#win").style.width = "0%"
@@ -79,18 +152,139 @@ function changeSceneStart(){
     document.querySelector("#start").style.display = "block"
     document.querySelector("#start").style.height = "100%"
     document.querySelector("#start").style.width = "100%"
-    console.log("hey")
 }
 
 function win(){
-    //Hide death screen
-    document.querySelector("#level1-screen").style.display = "none"
-    document.querySelector("#level1-screen").style.height = "0%"
-    document.querySelector("#level1-screen").style.width = "0%"
+    //Hide level 5
+    document.querySelector("#level5-screen").style.display = "none"
+    document.querySelector("#level5-screen").style.height = "0%"
+    document.querySelector("#level5-screen").style.width = "0%"
 
-    //Show start screen
+    //Show win screen
     document.querySelector("#win").style.display = "block"
     document.querySelector("#win").style.height = "100%"
     document.querySelector("#win").style.width = "100%"
 }
+
+function zeroClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "yellowgreen";
+    document.querySelector("#dp1").disabled = false;
+    document.querySelector("#dp3").disabled = false;
+    document.querySelector("#dp4").disabled = false;
+    document.querySelector("#dp5").disabled = false;
+    document.querySelector("#dp6").disabled = false;
+    document.querySelector("#dp8").disabled = false;
+    document.querySelector("#dp9").disabled = false;
+}
+
+function sixClicked(){
+    document.querySelector("#dp6").style.backgroundColor = "yellowgreen";
+    document.querySelector("#dp7").disabled = false;
+}
+
+function sevenClicked(){
+    document.querySelector("#dp7").style.backgroundColor = "yellowgreen";
+    document.querySelector("#dp2").disabled = false;
+}
+
+function twoClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp2").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+    win();
+}
+
+function oneClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+}
+
+function threeClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+}
+
+function fourClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+}
+
+function fiveClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+}
+
+function eightClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+}
+
+function nineClicked(){
+    document.querySelector("#dp0").style.backgroundColor = "transparent";
+    document.querySelector("#dp6").style.backgroundColor = "transparent";
+    document.querySelector("#dp7").style.backgroundColor = "transparent";
+    document.querySelector("#dp1").disabled = true;
+    document.querySelector("#dp3").disabled = true;
+    document.querySelector("#dp4").disabled = true;
+    document.querySelector("#dp5").disabled = true;
+    document.querySelector("#dp6").disabled = true;
+    document.querySelector("#dp7").disabled = true;
+    document.querySelector("#dp8").disabled = true;
+    document.querySelector("#dp9").disabled = true;
+}
+
 //make on hover function for all doors to know that there is a clickable element
