c00625fb52bdb4c1607bb60b58a07fc27decef0c
nigel-tay <ninenigel@gmail.com>
2021-05-11 11:18:40 +0800

    Finish layout for Level 6

--- js/main.js
+++ js/main.js
@@ -1,89 +1,96 @@
-//door mechanic
-
-//start to level 1
+// ===================================== START =======================================
+//Start to level 1
 document.querySelector("#entrance").addEventListener("click", changeSceneLevel1);
 
+// ===================================== LEVEL 1 =====================================
+//Level 1 to Death Screen
+document.querySelector("#not-scary-at-all").addEventListener("click", death);
+document.querySelector("#scary").addEventListener("click", death);
+
+//Level 1 to next room
+document.querySelector("#very-scary").addEventListener("click", changeSceneLevel2);
+
+// ===================================== LEVEL 2 =====================================
+// document.getElementById("#stair1").addEventListener("click", win);
+//Win to start
+document.querySelector(".win-to-start").addEventListener("click", changeSceneStart);
+
+// ===================================== DEATH =======================================
+//If either "Back to start" or "Quick Restart" was selected
+document.querySelector(".back-to-start").addEventListener("click", changeSceneStart);
+document.querySelector(".quick-restart").addEventListener("click", changeSceneLevel1);
+//FUNCTIONS
 function changeSceneLevel1(){
 
-    //show new scene//
-    //start to hide
+    //Hide start screen
     document.querySelector("#start").style.display = "none"
     document.querySelector("#start").style.height = "0%"
     document.querySelector("#start").style.width = "0%"
 
-    //death to hide
+    //Hide death screen
     document.querySelector("#death-display").style.display = "none"
     document.querySelector("#death-display").style.height = "0%"
     document.querySelector("#death-display").style.width = "0%"
 
-    //level 1 to show
+    //Show level 1
     document.querySelector("#level1-screen").style.display = "block"
     document.querySelector("#level1-screen").style.height = "100%"
     document.querySelector("#level1-screen").style.width = "100%"
-
-    // document.querySelector("#scenes").innerHTML =
-    //     "<div id=\"level\">\n" +
-    //     "   <div id=\"level1-screen\" class=\"container\">\n" +
-    //     "       <div><button id=\"not-scary-at-all\" class=\"door\"></button></div>\n" +
-    //     "       <div><button id=\"scary\" class=\"door\"></button></div>\n" +
-    //     "       <div><button id=\"very-scary\" class=\"door\"></button></div>\n" +
-    //     "   </div>\n" +
-    //     "</div>";
 }
 
-//Level 1 to death
-document.querySelector("#not-scary-at-all").addEventListener("click", death);
-document.querySelector("#scary").addEventListener("click", death);
+function changeSceneLevel2(){
 
-function death(){
-
-    //level 1 to hide
+    //Hide start screen
     document.querySelector("#level1-screen").style.display = "none"
     document.querySelector("#level1-screen").style.height = "0%"
     document.querySelector("#level1-screen").style.width = "0%"
 
-    //death to show
+    //Show level 1
+    document.querySelector("#level2-screen").style.display = "block"
+    document.querySelector("#level2-screen").style.height = "100%"
+    document.querySelector("#level2-screen").style.width = "100%"
+}
+
+function death(){
+
+    //Hide level 1
+    document.querySelector("#level1-screen").style.display = "none"
+    document.querySelector("#level1-screen").style.height = "0%"
+    document.querySelector("#level1-screen").style.width = "0%"
+
+    //Show death
     document.querySelector("#death-display").style.display = "block"
     document.querySelector("#death-display").style.height = "100%"
     document.querySelector("#death-display").style.width = "100%"
-
-
-    // document.querySelector("#scenes").innerHTML =
-    //     "<div id=\"death\" class=\"container\">\n" +
-    //     "        <div id=\"death-text\">\n" +
-    //     "            <h1>You've met the deadline...</h1>\n" +
-    //     "        </div>\n" +
-    //     "\n" +
-    //     "        <div class=\"head-container\">\n" +
-    //     "            <div id=\"head\">\n" +
-    //     "                <div class=\"eye\" id=\"left\"></div>\n" +
-    //     "                <div class=\"eye\" id=\"right\"></div>\n" +
-    //     "                <div id=\"nose\"></div>\n" +
-    //     "                <div id=\"mouth\"></div>\n" +
-    //     "            </div>\n" +
-    //     "        </div>\n" +
-    //     "\n" +
-    //     "        <div id=\"button-container\">\n" +
-    //     "            <button id=\"back-to-start\">Back to Start</button>\n" +
-    //     "            <button id=\"quick-restart\">Quick Restart</button>\n" +
-    //     "        </div>\n" +
-    //     "    </div>";
 }
 
-//death to start or level 1
-document.querySelector("#back-to-start").addEventListener("click", changeSceneStart);
-document.querySelector("#quick-restart").addEventListener("click", changeSceneLevel1);
-
 function changeSceneStart(){
-    //death to hide
+    //Hide death screen
     document.querySelector("#death-display").style.display = "none"
     document.querySelector("#death-display").style.height = "0%"
     document.querySelector("#death-display").style.width = "0%"
 
-    //start to show
+    //Hide win screen
+    document.querySelector("#win").style.display = "none"
+    document.querySelector("#win").style.height = "0%"
+    document.querySelector("#win").style.width = "0%"
+
+    //Show start screen
     document.querySelector("#start").style.display = "block"
     document.querySelector("#start").style.height = "100%"
     document.querySelector("#start").style.width = "100%"
+    console.log("hey")
 }
 
+function win(){
+    //Hide death screen
+    document.querySelector("#level1-screen").style.display = "none"
+    document.querySelector("#level1-screen").style.height = "0%"
+    document.querySelector("#level1-screen").style.width = "0%"
+
+    //Show start screen
+    document.querySelector("#win").style.display = "block"
+    document.querySelector("#win").style.height = "100%"
+    document.querySelector("#win").style.width = "100%"
+}
 //make on hover function for all doors to know that there is a clickable element
